981af1c862882523ebbd19d8aca97b6fb6cd29a4
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-04-28 19:03:46 +0000

    x\c

--- app/src/main/java/com/example/group_project_2/Login.java
+++ app/src/main/java/com/example/group_project_2/Login.java
@@ -1,7 +1,9 @@
 package com.example.group_project_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signUp (View view) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }

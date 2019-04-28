1b5c11bf9bd155a7c2f3e483f2f469a25ce649c8
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-04-28 20:37:26 +0000

    it worked i think

--- app/src/main/java/com/example/group_project_2/Login.java
+++ app/src/main/java/com/example/group_project_2/Login.java
@@ -4,17 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfile();
+            }
+        });
+
     }
 
-    public void signUp (View view) {
-        Intent intent = new Intent(this, SignUp.class);
+    public void openProfile () {
+        Intent intent = new Intent(this, UserProfile.class);
         startActivity(intent);
     }
+
+
 }

b0e8f83c6191ff58b07662fc19ba877bba2112e7
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-04-29 02:33:11 +0000

    it worked i think

--- app/src/main/java/com/example/group_project_2/Login.java
+++ app/src/main/java/com/example/group_project_2/Login.java
@@ -3,18 +3,30 @@ package com.example.group_project_2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
+    TextView loginuser;
     Button login;
+
+    DatabaseReference dbr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginuser = (TextView) findViewById(R.id.loginuser);
         login = (Button) findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +36,8 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        dbr = FirebaseDatabase.getInstance().getReference("users");
+
     }
 
     public void signUp(View view) {
@@ -32,9 +46,30 @@ public class Login extends AppCompatActivity {
     }
 
     public void openProfile () {
-        Intent intent = new Intent(this, Scan.class);
+        // Read from the database
+        dbr.orderByChild("username").equalTo(loginuser.getText().toString());
+        dbr.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                if (!dataSnapshot.exists()) {
+                    return;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+
+        Intent intent = new Intent(this, UserProfile.class);
         startActivity(intent);
     }
+
+
 
 
 }
--- app/src/main/java/com/example/group_project_2/UserProfile.java
+++ app/src/main/java/com/example/group_project_2/UserProfile.java
@@ -1,7 +1,14 @@
 package com.example.group_project_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void scanNow(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.setResultDisplayDuration(0);
+        integrator.setWide();  // Wide scanning rectangle, may work better for 1D barcodes
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+            //we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            // display it on screen
+            TextView scan_format = (TextView) findViewById(R.id.scan_format);
+            TextView scan_content = (TextView) findViewById(R.id.scan_content);
+            scan_format.setText("FORMAT: " + scanFormat);
+            scan_content.setText("CONTENT: " + scanContent);
+
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }

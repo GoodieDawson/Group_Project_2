e31293c294dfffb859e8463bdb4d019cc08e6977
lenryn <lenry.neequaye@ashesi.edu.gh>
2019-04-29 08:37:23 +0000

    mhhj

---
+++ app/src/main/java/com/example/group_project_2/activity_stake_user.java
@@ -0,0 +1,13 @@
+package com.example.group_project_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_stake_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stake_user);
+    }
+}
